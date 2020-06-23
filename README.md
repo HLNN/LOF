5c2ace3ff1752366d16ebc59bff590144f83770d
HLN <Huangln555@gmail.com>
2020-06-23 23:16:02 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Autoscript
+
+LOF每日溢价监控助手，用于每日定时检查LOF溢价情况，并自动将数据推送到微信。
+
+## 使用 GitHub Actions
+
+**推荐使用`GitHub Actions`的方式运行此程序**。`GitHub Actions`可以免费使用，不需要使用自己服务器。
+
+
+**注意**：不要直接在代码中修改`ServerChanKey`等敏感信息，公开仓库中的内容是任何人都可以查看的。**请使用secret功能储存敏感信息**。
+
+
+配置步骤：
+ - 点击右上角的`Fork`克隆这个仓库
+ - `Fork`的仓库默认禁用了`workflows`功能，需要手动打开：
+   - 点击上方的`Actions`选项卡
+   - 点击`I understand my workflows, go ahead and run them`
+ - 在仓库的`Settings`中设置[Server酱](http://sc.ftqq.com/3.version)的秘钥：
+   - 进入[Server酱](http://sc.ftqq.com/3.version)网站，使用GitHub登入，并绑定微信。
+   - 获得秘钥`SCKEY`
+   - 将秘钥以`ServerChanKey: SCKEY`的格式存入`【Settings】->【Secrets】`
+ - 任意修改`README.md`中的内容触发`Actions`，检查程序是否能正常运行
+
+## 自定义修改
+
+ - 自定义监控LOF列表：修改`lof.py`中的`LOFList`
+
+ - 自定义推送时间：修改`。github/workflows/lof.yml`中的`schedule`
+
+
+都看到这了，点个`Star`呗
--- lof.py
+++ lof.py
@@ -4,22 +4,10 @@ import re
 import pytz
 from datetime import datetime
 
+
 class LOF:
     def __init__(self):
-        self.session = requests.Session()
-        header = {
-            "Accept": "application/json, text/javascript, */*; q=0.01",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8,zh-CN;q=0.7,zh;q=0.6",
-            "Cache-Control": "no-cache",
-            "Connection": "keep-alive",
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "Host": "www.jisilu.cn",
-            "Origin": "https://www.jisilu.cn",
-            "Pragma": "no-cache",
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36",
-            "X-Requested-With": "XMLHttpRequest",
-            }
+        # 可注释掉不需要的数据
         self.s = {
             "代码": "fund_id",
             "名称": "fund_nm",
@@ -29,13 +17,17 @@ class LOF:
             # "实时估值": "estimate_value",
             "溢价率": "discount_rt",
         }
+        # 可在列表中添加想要监控的LOF
+        self.LOFList = [161005, 163402]
+        self.LOFList.sort()
+
+        self.session = requests.Session()
+        header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36",}
         self.session.headers.update(header)
         self.urlBase = "https://www.jisilu.cn/data/lof/detail/"
         self.urlLOF = "https://www.jisilu.cn/data/lof/stock_lof_list/?___jsl=LST___t="
 
         self.apiKey = "ServerChanKey"
-        self.LOFList = [161005, 163402]
-        self.LOFList.sort()
     
 
     def getInfo(self, id):
